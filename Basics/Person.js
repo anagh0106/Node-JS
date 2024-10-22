f3179bec6fd10777feed2f331464021776fee1cf
Anagh Patel <anagh0106@gmail.com>
2024-10-22 20:20:12 +0530

    Person Data Export

---
+++ Basics/Person.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Person File")
+
+const name="Anagh"
+const age=20
+
+const pHobby=()=>{
+    console.log("Hobby is Coding")
+}
+
+module.exports={
+    name,
+    age,
+    pHobby
+}
