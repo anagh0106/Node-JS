0ba8108a11b1e36f0f72f34b1f3c3aa03e34c2cb
Anagh Patel <anagh0106@gmail.com>
2024-10-23 14:46:10 +0530

    DB Connection

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-const express = require("express")
-const app = express()
-const PORT = 3000
-const persondata = require("./PersonData")
+const express=require("express")
+const app=express()
+const PORT=3000
 
+const dbconnection=require("./DB Connection/DB_Connection")
 
-app.get("/Persondata", (req, res) => {
-    console.log("Person Data Get API Invoked!.");
-    res.status(200).json({
-        Person_data: persondata.personArr,
-        msg: "Data Executed Successfully!."
-    })
-}) // function is used to get the data from server
+dbconnection.connection()
 
-
-// console.log(persondata.getDataByID(5))
-
-
-app.get("/Persondata/:id", (req, res) => {
-    const p_id = persondata.personArr.find((person) => person.id == req.params.id)
-
-    if (p_id) {
-        res.status(200).json({
-            Person_data: p_id
-        })
-    }
-    else {
-        res.status(404).json({
-            msg: "Person not found with the given id"
-        })
-    }
+app.get("/",(req,res)=>{
+    res.json("Home Page")
 })
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server Is Live On http://localhost:${PORT}`)
 })
+
