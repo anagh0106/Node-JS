2a358db37f7c90cd23b905e52579d829acf9e29c
Anagh Patel <anagh0106@gmail.com>
2024-10-29 11:25:35 +0530

    CRUD Operation

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express=require("express")
 const app=express()
 const PORT=3000
 
-const dbconnection=require("./DB Connection/DB_Connection")
+// db config
+const dbConnection = require("./util/DB_Connection")
+dbConnection.DbConnection()
 
-dbconnection.connection()
+
+app.use(express.json());    //get the user data in req.body as json format
+//Router
+const myrouter=require("./router/UserRouter")
+app.use("/users",myrouter)
 
 app.get("/",(req,res)=>{
-    res.json("Home Page")
+    res.json({
+        msg:"Hello Guys This Is Home Page"
+    })
 })
 
 
 app.listen(PORT,(req,res)=>{
-    console.log(`Server Is Live On http://localhost:${PORT}`)
-})
-
+    console.log(`Server Is Running On http://localhost:${PORT}`);
+    
+}) // it will listen those all activites on server
