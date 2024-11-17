6c0f40fe0a3a5fcbdc34861855b70ef37c249ba9
Anagh Patel <anagh0106@gmail.com>
2024-11-17 17:45:06 +0530

    App.js

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ const myrouter = require("./router/ProductRouter")
 const myrouter1 = require("./router/CategoryRouter")
 const empRouter = require("./router/EmployeeRouter")
 const student = require("./router/Studentrouter")
+const questionrouter = require("./router/QuestionRouter")
+const examrouter = require("./router/ExamRouter")
+
 app.use("/product", myrouter)
 app.use("/category", myrouter1)
 app.use("/employee", empRouter)
 app.use("/users", user)
 app.use("/student", student)
-
+app.use("/question", questionrouter)
+app.use("/exam", examrouter)
 
 app.listen(PORT, (req, res) => {
     console.log(`Server Is Running on http://localhost:${PORT}`)
